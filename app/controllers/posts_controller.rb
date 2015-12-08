b6e9f6de1abe58679cfc56d2f52d1c7f088ff545
Aviv Levinsky <pugsiman@gmail.com>
2015-12-08 03:47:18 +0200

    Optimization for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
+    @post = current_user.posts.new(post_params)
     if @post.save
       flash[:success] = 'Your post was created!'
       redirect_to root_path
